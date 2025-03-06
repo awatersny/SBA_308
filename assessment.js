525ca6155dbe64327246089aa7c43d577c0ff7d2
awatersny <awatersny@gmail.com>
2025-03-06 11:26:03 -0500

    List requirements for reference

--- assessment.js
+++ assessment.js
@@ -148,3 +148,18 @@ const LearnerSubmissions = [
 // Create a function named getLearnerData() that accepts these values as parameters, in the order listed: (CourseInfo, AssignmentGroup, [LearnerSubmission]), and returns the formatted result, which should be an array of objects as described above.
 
 // You may use as many helper functions as you see fit.
+
+// REQUIREMENTS!!!
+// TODO Declare variables properly using let and const where appropriate.
+// TODO Use operators to perform calculations on variables and literals.
+// TODO Use strings, numbers, and Boolean values cached within variables.
+// TODO Use at least two if/else statements to control program flow. Optionally, use at least one switch statement.
+// TODO Use try/catch statements to manage potential errors in the code, such as incorrectly formatted or typed data being fed into your program.
+// TODO Utilize at least two different types of loops.
+// TODO Utilize at least one loop control keyword such as break or continue.
+// TODO Create and/or manipulate arrays and objects.
+// TODO Demonstrate the retrieval, manipulation, and removal of items in an array or properties in an object.
+// TODO Use functions to handle repeated tasks.
+// TODO Program outputs processed data as described above. Partial credit will be earned depending on the level of adherence to the described behavior.
+// TODO Ensure that the program runs without errors (comment out things that do not work, and explain your blockers - you can still receive partial credit).
+// TODO Include a README file that contains a description of your application.
