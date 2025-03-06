f3eb21859dee19dc9bf646aef3c886d3dd1981e0
awatersny <awatersny@gmail.com>
2025-03-06 13:43:34 -0500

    Rename parameters for getLearnerData

--- assessment.js
+++ assessment.js
@@ -118,12 +118,19 @@ const LearnerSubmissions = [
 
 // End of provided data
 
+// If an assignment is not yet due, do not include it in the results or the average. Additionally, if the learner’s submission is late (submitted_at is past due_at), deduct 10 percent of the total points possible from their score for that assignment.
+//Find
+  // due date
+  // results
+  // average
+  // submitted: LearnerSubmissions[i].submission.submitted_at
 
-
+// TODO What if a value that you are expecting to be a number is instead a string? 
 // If an AssignmentGroup does not belong to its course (mismatching course_id), your program should throw an error, letting the user know that the input was invalid. Similar data validation should occur elsewhere within the program.
 try {
   if (AssignmentGroup.course_id === CourseInfo.id) {
 
+    // You should also account for potential errors in the data that your program receives. What if points_possible is 0? You cannot divide by zero.
     try {
       AssignmentGroup.assignments.forEach(assignment => {
         if (assignment.points_possible > 0) {
@@ -143,15 +150,13 @@ try {
 } catch (error) {
   console.log(error)
 }
-
-// You should also account for potential errors in the data that your program receives. What if points_possible is 0? You cannot divide by zero. What if a value that you are expecting to be a number is instead a string? 
-
 // Use try/catch and other logic to handle these types of errors gracefully.
 
-// If an assignment is not yet due, do not include it in the results or the average. Additionally, if the learner’s submission is late (submitted_at is past due_at), deduct 10 percent of the total points possible from their score for that assignment.
+
+
 
 // Create a function named getLearnerData() that accepts these values as parameters, in the order listed: (CourseInfo, AssignmentGroup, [LearnerSubmission]), and returns the formatted result, which should be an array of objects as described above.
-const getLearnerData = function(CourseInfo, AssignmentGroup, LearnerSubmissions){
+const getLearnerData = function(info, group, submissions){
   // Your goal is to analyze and transform this data such that the output of your program is an array of objects, each containing the following information in the following format:
   const output = []
   
