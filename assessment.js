0fc35c6c12a8454f44efcb43189c4350f6c3d161
awatersny <awatersny@gmail.com>
2025-03-06 11:04:04 -0500

    Copy schema from instructions

---
+++ assessment.js
@@ -0,0 +1,35 @@
+// Provided data below
+// const courseInfo = {
+//   "id": number,
+//   "name": string,
+// }
+
+// const assignmentGroup = {
+//   "id": number,
+//   "name": string,
+//   // the ID of the course the assignment group belongs to
+//   "course_id": number,
+//   // the percentage weight of the entire assignment group
+//   "group_weight": number,
+//   "assignments": [AssignmentInfo],
+// }
+
+// const assignmentInfo = {
+//   "id": number,
+//   "name": string,
+//   // the due date for the assignment
+//   "due_at": Date string,
+//   // the maximum points possible for the assignment
+//   "points_possible": number,
+// }
+
+// const learnerSubmission = 
+// {
+//     "learner_id": number,
+//     "assignment_id": number,
+//     "submission": {
+//       "submitted_at": Date string,
+//       "score": number
+//     }
+// }
+// End of provided data
