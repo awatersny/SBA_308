44faf12a02a3794d3f56b351a5ce824ce15268db
awatersny <awatersny@gmail.com>
2025-03-06 11:13:21 -0500

    Copy additional instructions

--- assessment.js
+++ assessment.js
@@ -33,3 +33,34 @@
 //     }
 // }
 // End of provided data
+
+
+// Your goal is to analyze and transform this data such that the output of your program is an array of objects, each containing the following information in the following format:
+
+// {
+    // the ID of the learner for which this data has been collected
+//     "id": number,
+    // the learner’s total, weighted average, in which assignments
+    // with more points_possible should be counted for more
+    // e.g. a learner with 50/100 on one assignment and 190/200 on another
+    // would have a weighted average score of 240/300 = 80%.
+//     "avg": number,
+    // each assignment should have a key with its ID,
+    // and the value associated with it should be the percentage that
+    // the learner scored on the assignment (submission.score / points_possible)
+//     <assignment_id>: number,
+    // if an assignment is not yet due, it should not be included in either
+    // the average or the keyed dictionary of scores
+// }
+
+// If an AssignmentGroup does not belong to its course (mismatching course_id), your program should throw an error, letting the user know that the input was invalid. Similar data validation should occur elsewhere within the program.
+
+// You should also account for potential errors in the data that your program receives. What if points_possible is 0? You cannot divide by zero. What if a value that you are expecting to be a number is instead a string? 
+
+// Use try/catch and other logic to handle these types of errors gracefully.
+
+// If an assignment is not yet due, do not include it in the results or the average. Additionally, if the learner’s submission is late (submitted_at is past due_at), deduct 10 percent of the total points possible from their score for that assignment.
+
+// Create a function named getLearnerData() that accepts these values as parameters, in the order listed: (CourseInfo, AssignmentGroup, [LearnerSubmission]), and returns the formatted result, which should be an array of objects as described above.
+
+// You may use as many helper functions as you see fit.
